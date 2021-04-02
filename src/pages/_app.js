306b486a958097c01dd5aad222e7d9de80fc763b
Evan Platzer <evan.platzer@gmail.com>
2021-04-02 06:54:25 -0400

    Extract theme and styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,6 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
-
-const GlobalStyle = createGlobalStyle`
-body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`;
-
-const theme = {
-    palette: {
-        primary: '#158e84',
-        secondary: '#9ccc66',
-        error: '#E44C65',
-        background: '#333333',
-        text: '#f6f7fc',
-    },
-};
+import { ThemeProvider } from 'styled-components';
+import theme from '../styles/theme';
+import GlobalStyle from '../styles/GlobalStyle';
 
 function MyApp({ Component, pageProps }) {
     return (
---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+const theme = {
+    palette: {
+        primary: '#158e84',
+        secondary: '#9ccc66',
+        error: '#E44C65',
+        background: '#333333',
+        text: '#f6f7fc',
+    },
+};
+
+export default theme;
