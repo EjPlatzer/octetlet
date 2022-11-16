26666678b6311e736dc5a3565f3ed6b8dcbbc858
Evan Platzer <evan.platzer@gmail.com>
2022-11-15 20:45:26 -0500

    Add about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Octetlet: Chemistry Review Web App.</h1>
+<p>
+	You can use it to create, share, and practice quizzes, flashcards, and interactive problems
+	relating to various domains of chemistry.
+</p>
